74658a4927dc8bed7ed2634bb9eaadee9653b106
FullSilence <HromovValentin@gmail.com>
2016-09-13 19:45:21 +0300

    Add files via upload

---
+++ TestTerraria/TestTerraria/Person.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading;
+
+namespace TestTerraria
+{
+    public class Person
+    {
+        private int x = 1;
+        private int y = 16;
+
+        Direction previousDirection;
+
+        public Point Location
+        {
+            get
+            {
+                return new Point( x,y );
+            }
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                Move(Direction.LEFT);
+            else if (key == ConsoleKey.RightArrow)
+                Move(Direction.RIGHT);
+            else if (key == ConsoleKey.UpArrow)
+                Move(Direction.UP);
+            else if (key == ConsoleKey.DownArrow)
+                Move(Direction.DOWN);
+            else if (key == ConsoleKey.Spacebar)
+                Jump();
+            else if (key == ConsoleKey.Escape)
+                Environment.Exit(0);
+        }
+
+        private void Jump()
+        {
+            clear();
+            y--;
+            Draw();
+
+            Thread.Sleep(10);
+            Move(previousDirection);
+            Thread.Sleep(10);
+
+            clear();
+            y--;
+            Draw();
+
+            Thread.Sleep(50);
+            Move(previousDirection);
+            Thread.Sleep(50);
+            clear();
+            y++;
+            Draw();
+
+            Thread.Sleep(50);
+            Move(previousDirection);
+            Thread.Sleep(50);
+            clear();
+            y++;
+            Draw();
+
+            Thread.Sleep(10);
+            Move(previousDirection);
+            Thread.Sleep(10);
+
+            previousDirection = Direction.STOP;
+        }
+
+        private void clear()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+        }
+
+        private bool Move(Direction direction)
+        {
+            previousDirection = direction;
+
+            clear();
+
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    x--;
+                    break;
+                case Direction.RIGHT:
+                    x++;
+                    break;
+                case Direction.UP:
+                    y--;
+                    break;
+                case Direction.DOWN:
+                    y++;
+                    break;
+                default:
+                    return false;
+            }            
+
+            return true;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write('*');
+        }
+    }
+}
---
+++ TestTerraria/TestTerraria/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestTerraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Chunk chunk = new Chunk(new Point(0, 16), new Point(120, 1));
+            chunk.Draw();
+
+
+            while (true)
+            {
+                person.HandleKey(Console.ReadKey().Key);
+
+                if (chunk.Collision(person.Location))
+                {
+                    Program.Exit();
+                }
+
+
+                person.Draw();
+            }
+        }
+
+        private static void Exit()
+        {
+            Console.Clear();
+            Console.WriteLine("Игр окончена! Намите Enter");
+
+            Console.ReadLine();
+
+            Environment.Exit(0);
+        }
+    }
+
+
+
+
+
+    class Chunk
+    {
+        private Point topLeft;
+
+        private Point bottomRight;
+
+
+        internal Point TopLeft
+        {
+            get
+            {
+                return topLeft;
+            }
+
+            set
+            {
+                topLeft = value;
+            }
+        }
+
+        internal Point BottomRight
+        {
+            get
+            {
+                return bottomRight;
+            }
+
+            set
+            {
+                bottomRight = value;
+            }
+        }
+
+        public List<Point> pointList = new List<Point>();
+
+        public Chunk()
+        {
+        }
+
+        public Chunk(Point topLeft, Point bottomRight)
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+
+            for (int x = topLeft.X; x < bottomRight.X; x++)
+            {
+                PerlinNoise noise = new PerlinNoise(134243432L);
+
+                pointList.Add(new Point(x, noise.getNoise(x, 28)));
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (var point in pointList)
+            {
+                Console.SetCursorPosition(point.X, point.Y);
+                Console.Write("#");
+            }
+        }
+
+        public bool Collision(Point location)
+        {
+            foreach (var point in pointList)
+            {
+                if (point.X == location.X && point.Y == location.Y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
